121d53541962c799cf51012733999a71f32d84f4
Hayk Ghonakhchyan <hayk.ghonakhchyan@EFU.local>
2019-07-25 16:41:03 +0400

    Added few components

---
+++ frontend/components/Nav.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const Nav = () => (
+  <div>
+    <Link href="/sell">
+      <a>Go to sell</a>
+    </Link>
+    <Link href="/">
+      <a>Go to home</a>
+    </Link>
+  </div>
+);
+
+export default Nav;
